714f25e216d5513de12d0de58758efdf69b6e48a
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-08-04 20:59:56 +0100

    test render New

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -4,20 +4,27 @@ import { Theme } from '@radix-ui/themes'
 import userEvent from '@testing-library/user-event'
 
 describe('OrderStatusSelector', () => {
-  it('should render New as the default value', () => {
+  const renderComponent = () => {
     render(
       <Theme>
         <OrderStatusSelector onChange={vi.fn()} />
       </Theme>
-    ) //vi.fn is a mock function
-
+    )
+    // vi.fn is a mock function
     // ? OrderStausSelecor uses RadixUI
     // ? Error: `useThemeContext` must be used within a `Theme`
 
+    return {
+      trigger: screen.getByRole('combobox'),
+    }
+  }
+
+  it('should render New as the default value', () => {
+    const { trigger } = renderComponent()
+
     // ? ReferenceError: ResizeObserver is not defined - a browser API that is not available in Node environment and JSDOM doesn't provide this at this minute. package needed is resize-observer-polyfill and configured in setup.ts
 
-    const button = screen.getByRole('combobox')
-    expect(button).toHaveTextContent(/new/i)
+    expect(trigger).toHaveTextContent(/new/i)
   })
 
   it('should render correct statuses', async () => {
