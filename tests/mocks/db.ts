38b3726753a19725f0630dba711e8480a4b35844
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-08-12 21:48:47 +0100

    rafactor

--- tests/mocks/db.ts
+++ tests/mocks/db.ts
@@ -15,3 +15,9 @@ export const db = factory({
     category: oneOf('category')
   }
 })
+
+export const getProductsByCategory = (categoryId: number) => db.product.findMany({
+  where: {
+    categoryId: { equals: categoryId }
+  }
+})
